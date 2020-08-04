fba3e38b58fb7762838c559f575226ba41edd03b
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-08-04 10:37:37 -0400

    Solution to exercise 3

--- src/components/SeatContext.js
+++ src/components/SeatContext.js
@@ -4,19 +4,30 @@ export const SeatContext = createContext();
 
 const initialState = {
   hasLoaded: false,
-  seats: null,
+  bookedSeats: null,
   numOfRows: 0,
   seatsPerRow: 0,
 };
 
 function reducer(state, action) {
-  // TODO
+  switch (action.type) {
+    case 'receive-seat-info-from-server':
+      return {
+        ...state,
+        hasLoaded: true,
+        bookedSeats: action.bookedSeats,
+        numOfRows: action.numOfRows,
+        seatsPerRow: action.seatsPerRow,
+      };
+    default:
+      throw new Error(`Unrecognized action: ${action.type}`);
+  }
 }
 
-export const SeatProvider = ({ children }) => {
+export function SeatProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const receiveSeatInfoFromServer = (data) => {
+  const receiveSeatInfoFromServer = data => {
     dispatch({
       type: "receive-seat-info-from-server",
       ...data,
